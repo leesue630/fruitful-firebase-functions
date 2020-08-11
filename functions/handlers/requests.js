ebdc085bac8dcebf76f4dea5c930dabe177ba59f
leesue630 <lee.sue630@gmail.com>
2020-08-11 01:15:06 -0400

    fixed email userHandle bug

--- functions/handlers/requests.js
+++ functions/handlers/requests.js
@@ -2,7 +2,7 @@ const { db } = require("../util/admin");
 
 exports.makeRequest = (req, res) => {
   db.collection("fruit-requests")
-    .add({ user: req.user.handle, fruit: req.body.fruit })
+    .add({ userHandle: req.user.handle, fruit: req.body.fruit })
     .then((doc) => {
       return res.json({
         message: `document ${doc.id} created successfully`,
